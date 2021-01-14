12b5e1fcaeb1c13f2d2cf0a88ca53bbebec6a366
Pablo Martinez <pmirazabal@gmail.com>
2021-01-14 19:08:22 +0100

    Initial commit

---
+++ desktop/src/com/pgdx/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.pgdx.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pgdx.drop.DropGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new DropGame(), config);
+	}
+}
